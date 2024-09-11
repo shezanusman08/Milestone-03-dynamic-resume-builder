afaca08b0d6617cc0c1bde882e62a074358d8273
shezanusman08 <164097373+shezanusman08@users.noreply.github.com>
2024-09-12 03:54:22 +0500

    improvement

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ var _a;
     var nameElement = document.getElementById("name");
     var emailElement = document.getElementById("email");
     var phoneElement = document.getElementById("contact");
+    var adrElement = document.getElementById("adr");
     var eduElement = document.getElementById("edu");
     var expElement = document.getElementById("exp");
     var skillElement = document.getElementById("skill");
-    if (nameElement && emailElement && phoneElement && eduElement && expElement && skillElement) {
+    var aboutElement = document.getElementById("about");
+    if (nameElement && emailElement && phoneElement && adrElement && eduElement && expElement && skillElement && aboutElement) {
         var name_1 = nameElement.value;
         var email = emailElement.value;
         var phone = phoneElement.value;
+        var adrress = adrElement.value;
         var education = eduElement.value;
         var exp = expElement.value;
         var skill = skillElement.value;
+        var about = aboutElement.value;
         //Get Output
-        var resumeOutput = "\n<h2>Resume</h2>\n<p><strong>Name:</strong>".concat(name_1, "</p>\n<p><strong>Email:</strong>").concat(email, "</p>\n<p><strong>Phone Number:</strong>").concat(phone, "</p>\n\n<h3>Education:</h3>\n<p>").concat(education, "</p>\n\n<h3>Experience:</h3>\n<p>").concat(exp, "</p>\n\n<h3>Skills:</h3>\n<p>").concat(skill, "</p>\n");
+        var resumeOutput = "\n<h2>Resume</h2>\n<p><strong>Name: </strong>".concat(name_1, "</p>\n<p><strong>Email: </strong>").concat(email, "</p>\n<p><strong>Phone Number: </strong>").concat(phone, "</p>\n<p><strong>Country/City: </strong>").concat(adrress, "</p>\n\n<h3>Objective: </h3>\n<p>").concat(about, "</p>\n\n<h3>Education: </h3>\n<p>").concat(education, "</p>\n\n<h3>Experience: </h3>\n<p>").concat(exp, "</p>\n\n<h3>Skills: </h3>\n<p>").concat(skill, "</p>\n\n");
         var resumeOutputElement = document.getElementById("resumeOutput");
         if (resumeOutputElement) {
             resumeOutputElement.innerHTML = resumeOutput;
--- script.ts
+++ script.ts
@@ -6,41 +6,54 @@ document.getElementById("resumeForm")?.addEventListener('submit', function(event
 
 
 // Get Input
+
 const nameElement = document.getElementById("name") as HTMLInputElement;
 const emailElement = document.getElementById("email") as HTMLInputElement;
 const phoneElement = document.getElementById("contact") as HTMLInputElement;
+const adrElement = document.getElementById("adr") as HTMLInputElement;
 const eduElement = document.getElementById("edu") as HTMLInputElement;
 const expElement = document.getElementById("exp") as HTMLInputElement;
 const skillElement = document.getElementById("skill") as HTMLInputElement;
+const aboutElement = document.getElementById("about") as HTMLInputElement;
 
 
 
-if(nameElement && emailElement && phoneElement && eduElement && expElement && skillElement){
+
+
+if(nameElement && emailElement && phoneElement && adrElement && eduElement && expElement && skillElement && aboutElement){
 
     const name = nameElement.value;
     const email = emailElement.value;
     const phone = phoneElement.value;
+    const adrress = adrElement.value;
     const education = eduElement.value;
     const exp = expElement.value;
     const skill = skillElement.value;
+    const about = aboutElement.value;
+
 
 
     
 //Get Output
 const resumeOutput = `
 <h2>Resume</h2>
-<p><strong>Name:</strong>${name}</p>
-<p><strong>Email:</strong>${email}</p>
-<p><strong>Phone Number:</strong>${phone}</p>
+<p><strong>Name: </strong>${name}</p>
+<p><strong>Email: </strong>${email}</p>
+<p><strong>Phone Number: </strong>${phone}</p>
+<p><strong>Country/City: </strong>${adrress}</p>
 
-<h3>Education:</h3>
+<h3>Objective: </h3>
+<p>${about}</p>
+
+<h3>Education: </h3>
 <p>${education}</p>
 
-<h3>Experience:</h3>
+<h3>Experience: </h3>
 <p>${exp}</p>
 
-<h3>Skills:</h3>
+<h3>Skills: </h3>
 <p>${skill}</p>
+
 `;
 
 
